e047dc9b6ac2b14ee4e491b2f78ca7e0e6d764de
The Abattoir <abattoir@abattoir.in>
2009-03-18 23:58:07 +0530

    Now shipping with openlayers!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @title = 'Home'
+    @onload = 'init();'
   end
 end
--- app/controllers/home_controller.rb~
+++ app/controllers/home_controller.rb~
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @title = 'Home'
   end
 end
