e2a8a41e4c02e7c75ffa0bd63d93d412731e59b1
The Abattoir <abattoir@abattoir.in>
2009-04-04 00:45:21 +0530

    More work on the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -40,4 +40,5 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing the them or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.connect ':controller/:action.:format'
 end
