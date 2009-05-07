5f3653e28c7743a9208e5bcc5eb84db8c6accd38
Yuvi <yuvipanda@gmail.com>
2009-05-08 00:37:34 +0530

    Marginal Support for Revision History

--- appmodels/models.py
+++ appmodels/models.py
@@ -7,6 +7,17 @@ class Stage(models.Model):
 	mtc_name = models.CharField(max_length=255)
 	#routes = models.ManyToManyField('Route') #Since Route isn't yet defined
 
+	def save(self, comment=""):
+		sh = StageRevision(
+							stage = self, 
+							display_name = self.display_name,
+							latitude = self.latitude,
+							longitude = self.longitude,
+							comment = comment
+						  )
+		sh.save()												
+		super(Stage, self).save()
+
 class Route(models.Model):
 	display_name = models.CharField(max_length=64)
 	mtc_name = models.CharField(max_length=64)
@@ -21,4 +32,11 @@ class RouteStage(models.Model):
 	route = models.ForeignKey(Route)
 	stage = models.ForeignKey(Stage)
 	sequence = models.IntegerField()
-# Create your models here.
+
+class StageRevision(models.Model):
+	stage = models.ForeignKey(Stage)
+	edited_at = models.DateTimeField(auto_now_add=True)
+	display_name = models.CharField(max_length=255)
+	latitude = models.FloatField(null=True, blank=True)
+	longitude = models.FloatField(null=True, blank=True)
+	comment = models.CharField(max_length=1024, blank=True, null=True)
--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os.path
 
 ROOT_DIR = os.path.dirname(__file__)
 
-DEBUG = False 
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
