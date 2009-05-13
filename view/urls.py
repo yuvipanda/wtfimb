ee145d40491818d4709700a533b06a5ec1295b5b
The Abattoir <abattoir@abattoir.in>
2009-05-14 01:15:56 +0530

    Fix for #38

--- view/urls.py
+++ view/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('wtfimb.view.views',
-		(r'^$', 'index'),
-		(r'route/(?P<id>\d+)/$', 'show_route'),
-		(r'stage/(?P<id>\d+)/$', 'show_stage'),
-		(r'unmapped/stages', 'show_unmapped_stages'),
-		)
+        (r'^$', 'index'),
+        (r'route/(?P<id>\d+)/$', 'show_route'),
+        (r'stage/(?P<id>\d+)/$', 'show_stage'),
+        (r'unmapped/stages', 'show_unmapped_stages'),
+        (r'mapped/stages', 'show_mapped_stages')
+        )
--- view/views.py
+++ view/views.py
@@ -4,18 +4,23 @@ from appmodels.models import *
 from django.views.generic.simple import direct_to_template
 
 def index(request):
-	pass
+    pass
 
 def show_route(request, id):
-	r = Route.objects.get(id=id)
-	return direct_to_template(request, 'show_route.html', {'route':r})
+    r = Route.objects.get(id=id)
+    return direct_to_template(request, 'show_route.html', {'route':r})
 
 def show_stage(request, id):
-	s = Stage.objects.get(id=id)
-	return	direct_to_template(request, 'show_stage.html', {'stage':s})
+    s = Stage.objects.get(id=id)
+    return  direct_to_template(request, 'show_stage.html', {'stage':s})
 
 def show_unmapped_stages(request):
-	unmapped = Stage.objects.filter(latitude=None)
-	return render_to_response(request,"show_unmapped_stages.html", 
-			{'unmapped_stages':unmapped})
+    unmapped = Stage.objects.filter(latitude=None)
+    return direct_to_template(request,"show_unmapped_stages.html", 
+            {'unmapped_stages':unmapped})
+
+def show_mapped_stages(request):
+    mapped = Stage.objects.filter(latitude__isnull=False)
+    return direct_to_template(request, "show_mapped_stages.html",
+            {'mapped_stages':mapped})
 
