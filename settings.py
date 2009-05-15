2b0e4cdc73b0c3e8dde391cc6a789f080fe65c34
Yuvi <yuvipanda@gmail.com>
2009-05-16 01:18:04 +0530

    Fixed settings file to point to new admin-media

--- settings.py
+++ settings.py
@@ -53,7 +53,7 @@ STATIC_DOC_ROOT = os.path.join(ROOT_DIR, 'static').replace('\\','/')
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/admin-media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'hs#xo8_e6hxi747-vz_7!=xzui*!$#ds3)x-hxfpfqc704edxd'
