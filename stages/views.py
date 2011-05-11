e281eaae6208657cd87b0cb7200ba1b5ada71c74
Kishore kumar J <jkk.2k9@gmail.com>
2011-05-11 21:48:01 +0530

    Improved stop page to show nearby stops(2kms radius)

--- stages/views.py
+++ stages/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse,HttpResponseRedirect, Http404
 from models import *
 from django.views.generic.simple import direct_to_template
 from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import D
 from django.shortcuts import get_object_or_404
 
 from forms import EditStageForm
@@ -27,7 +28,21 @@ def show_stage(request, city, id):
             )
         else:
             form = EditStageForm()
-        return direct_to_template(request, 'stages/show_stage.html', {'form':form, 'city': city, 'stage':s})
+        default_map = {'zoom':12}
+        if city == "chennai":
+            default_map["lat"] = 13.0456;
+            default_map["lon"] = 80.232;
+        elif city == "bangalore":
+            default_map["lat"] = 12.9832;
+            default_map["lon"] = 77.5915;
+        elif city == "delhi":
+            default_map["lat"] = 28.6038;
+            default_map["lon"] = 77.2134;
+        else:
+            default_map["lat"] = 13.0456;
+            default_map["lon"] = 80.232;
+        nearby_stages = Stage.objects.filter(city=city).filter(location__distance_lte=(s.location, D(km=2))).exclude(pk=s.id)
+        return direct_to_template(request, 'stages/show_stage.html', {'form':form, 'city': city, 'default_map': default_map, 'stage':s, 'nearby_stages':nearby_stages})
 
 def show_unmapped_stages(request, city):
     unmapped = Stage.objects.filter(city=city).filter(location=None)
